060e6b3841fa64e735579ad54d397af35c52fdf7
kukuxer <nazardaun5551@gmail.com>
2024-01-01 13:47:35 +0000

    Abobus

---
+++ src/GraphicalCalculator.java
@@ -0,0 +1,100 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GraphicalCalculator extends JFrame {
+    private JTextField textField;
+    private double num1 = 0, num2 = 0, result = 0;
+    private char operator = ' ';
+
+    public GraphicalCalculator() {
+        setTitle("Sheniator");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        getContentPane().setBackground(Color.BLACK);
+
+        textField = new JTextField();
+        textField.setEditable(false);
+        textField.setBackground(Color.BLACK);
+        textField.setForeground(Color.WHITE);
+        textField.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        textField.setFont(new Font("Arial", Font.PLAIN, 48));
+        textField.setPreferredSize(new Dimension(300, 50));
+        add(textField, BorderLayout.NORTH);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(4, 4, 5, 5));
+        buttonPanel.setBackground(Color.BLACK);
+
+        String[] buttonLabels = {
+                "7", "8", "9", "/",
+                "4", "5", "6", "*",
+                "1", "2", "3", "-",
+                "0", ".", "=", "+"
+        };
+
+        for (String label : buttonLabels) {
+            JButton button = new JButton(label);
+            button.addActionListener(new ButtonClickListener());
+            button.setBackground(Color.BLACK);
+            button.setForeground(Color.WHITE);
+            button.setBorder(BorderFactory.createLineBorder(Color.WHITE));
+            button.setFont(new Font("Arial", Font.PLAIN, 40));
+            buttonPanel.add(button);
+        }
+
+        add(buttonPanel, BorderLayout.CENTER);
+
+        pack();
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+    }
+
+    private class ButtonClickListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            JButton source = (JButton) e.getSource();
+            String buttonText = source.getText();
+
+            if (buttonText.matches("[0-9]")) {
+                textField.setText(textField.getText() + buttonText);
+            } else if (buttonText.matches("[\\+\\-\\*/]")) {
+                num1 = Double.parseDouble(textField.getText());
+                operator = buttonText.charAt(0);
+                textField.setText("");
+            } else if (buttonText.equals("=")) {
+                num2 = Double.parseDouble(textField.getText());
+                switch (operator) {
+                    case '+':
+                        result = num1 + num2;
+                        break;
+                    case '-':
+                        result = num1 - num2;
+                        break;
+                    case '*':
+                        result = num1 * num2;
+                        break;
+                    case '/':
+                        if (num2 != 0) {
+                            result = num1 / num2;
+                        } else {
+                            textField.setText("Error");
+                            return;
+                        }
+                        break;
+                }
+                textField.setText(String.valueOf(result));
+            } else if (buttonText.equals(".")) {
+                if (!textField.getText().contains(".")) {
+                    textField.setText(textField.getText() + buttonText);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new GraphicalCalculator());
+    }
+}
